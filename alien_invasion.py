11edd8588e455dbcd9ebf4b964ee96cd53b853dc
DesBlyum <desblyum@ya.ru>
2023-02-04 20:48:09 +0400

    alien_invasion.py - добавлен полноэкранный режим;                     выход при нажатии клавиши ESC;                     рефакторинг. ship.py - изменено перемещение корабля не на 1, а на значение из переменной ship_speed           Сохранение вещественной координаты центра корабля

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,9 +11,14 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
-        pygame.display.set_caption("Инопланетное вторжение (покаритель космоса - МАРКа)")
+        #self.screen = pygame.display.set_mode(
+        #    (self.settings.screen_width, self.settings.screen_height))
+
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
+        pygame.display.set_caption("Инопланетное вторжение (охотник на пришельцев)")
 
         # назначение цвета фона.
         self.bg_color = (self.settings.bg_color)
@@ -34,19 +39,29 @@ class AlienInvasion:
             if evant.type == pygame.QUIT:
                 sys.exit()
             elif evant.type == pygame.KEYDOWN:
-                if evant.key == pygame.K_RIGHT:
-                    #переместить корабль вправо пока нажата клафища
-                    self.ship.moving_right = True
-                elif evant.key == pygame.K_LEFT:
-                    # переместить корабль вправо пока нажата клафища
-                    self.ship.moving_left = True
+                self._check_keydown_evants(evant)
             elif evant.type == pygame.KEYUP:
-                if evant.key == pygame.K_RIGHT:
-                    #клавища вправо - отжали, более не перемещаем корабль
-                    self.ship.moving_right = False
-                elif evant.key == pygame.K_LEFT:
-                    #клавища вправо - отжали, более не перемещаем корабль
-                    self.ship.moving_left = False
+                self._check_keyup_evants(evant)
+
+    def _check_keydown_evants(self, evant):
+        """Реагирует на нажатие клавиш"""
+        if evant.key == pygame.K_RIGHT:
+            # переместить корабль вправо пока нажата клафища
+            self.ship.moving_right = True
+        elif evant.key == pygame.K_LEFT:
+            # переместить корабль вправо пока нажата клафища
+            self.ship.moving_left = True
+        elif evant.key == pygame.K_ESCAPE:
+            sys.exit()
+
+    def _check_keyup_evants(self, evant):
+        """Реагирует на отпускание клавиш"""
+        if evant.key == pygame.K_RIGHT:
+            # клавища вправо - отжали, более не перемещаем корабль
+            self.ship.moving_right = False
+        elif evant.key == pygame.K_LEFT:
+            # клавища вправо - отжали, более не перемещаем корабль
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """Обновляет изображение на экране и отображает новый экран"""
